68c1a9a94217e61460d54d1b7d56c7ee3e22f554
Flávio Silveira <flavioaugustosilveira@gmail.com>
2019-01-11 00:57:53 -0200

    Adding Vundle Install/Remove

--- scripts/remove-vim-config.sh
+++ scripts/remove-vim-config.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-printf "Removing VIM config..."
+printf "Removing VIM config...\n"
 rm -rf ~/.vimrc
-printf ".vimrc file removed!\n"
+printf ".vimrc file removed!\n\n"
+
+printf "Removing Vundle and Plugins...\n"
+rm -rf ~/.vim/bundle
+printf "Vundle and Plugins removed!\n\n"
--- scripts/vim-config.sh
+++ scripts/vim-config.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+# .vimrc file
 if [ -e ~/.vimrc -a "$1" != "-f" ]
 then
-  printf "Oops! You already have a .vimrc file in your home folder!\nUse -f as last parameter to force an update! Use the --vimrc option if you just want to update the vimrc file.\n\n" 
+  printf "Oops! You already have a .vimrc file in your home folder!\nUse -f as last parameter to force an update!\n\n" 
 else
   printf "Installing VIM configs...\n"
   cp files/vimrc ~/.vimrc
   chmod 644 ~/.vimrc
-  printf ".vimrc file installed! Set permission 644...done!\n"
-
-  # Install Vundle
-  # Install Plugins
+  printf ".vimrc file Installed! Set permission 644...done!\n\n"
 fi
+
+# Install Vundle and Plugins
+if [ -d ~/.vim/bundle/Vundle.vim -a "$1" != "-f" ]
+then
+  printf "Oops! You already have a Vundle Setup started and/or some Plugins!\nUse -f as last parameter to force an update!\n\n" 
+else
+  rm -rf ~/.vim/bundle
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  printf "Vundle Installed!\n"
+
+  printf "Running Plugins Installation...\n"
+  vim +PluginInstall +qall
+  printf "Plugins Installation Complete!\n\n"
+fi
+
+
+
