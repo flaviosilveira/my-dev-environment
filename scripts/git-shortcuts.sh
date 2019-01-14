9d7464843c27e0377bda31dd764174bb48911d41
Flávio Silveira <flavioaugustosilveira@gmail.com>
2019-01-14 12:01:41 -0200

    Adding git shortcuts install/remove

--- scripts/git-shortcuts.sh
+++ scripts/git-shortcuts.sh
@@ -1,6 +1,35 @@
+#!/bin/bash
 
-#alias gc='git clone'
-#alias gs='git status'
-#alias ga='git add'
-#alias gc='git commit'
-#alias gpo='git push origin master'
+if [ ! -e ~/.bash_profile ]
+then
+  printf "Cant find .bash_profile file.. creating one..\n"
+  touch ~/.bash_profile
+  chmod 644 ~/.bash_profile
+  printf ".bash_profile created!\n\n"
+fi
+
+
+if  [[ $(command grep -il "git commit" ~/.bash_profile) != "" ]] && [[ "$1" != "-f" ]]
+then
+  printf "Looks like you already have some git shortcuts in your bash profile..\nUse -f to remove them and re-add them.\n\n"
+else
+
+  if [[ $(command grep -il "git commit" ~/.bash_profile) != "" ]]
+  then
+    printf "Removing actual git shortcuts..."
+    ./scripts/remove-git-shortcuts.sh
+  fi
+
+  printf "Creating shortcuts...\n"
+  echo "" >> ~/.bash_profile
+  echo "## Git Shortcuts ###" >> ~/.bash_profile
+  echo "alias gs='git status'" >> ~/.bash_profile
+  echo "alias ga='git add'" >> ~/.bash_profile
+  echo "alias gc='git commit'" >> ~/.bash_profile
+  echo "alias gpo='git push origin master'" >> ~/.bash_profile
+  printf "Shortcuts created!\n\n"
+
+  printf "Reloading .bash_profile\n"
+  source ~/.bash_profile
+  printf "Done!\n\n"
+fi
---
+++ scripts/remove-git-shortcuts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ ! -e ~/.bash_profile ]
+then
+  printf "You not even have a bash_profile file! All done here!\n\n"
+fi
+
+if  [[ $(command grep -il "git commit" ~/.bash_profile) = "" ]]
+then
+  printf "Cant find git shortcuts... All done here!\n\n"
+fi
+
+printf "Removing git shortcuts..."
+sed -i '' '/Git Shortcuts/d' ~/.bash_profile
+sed -i '' '/git status/d' ~/.bash_profile
+sed -i '' '/git add/d' ~/.bash_profile
+sed -i '' '/git commit/d' ~/.bash_profile
+sed -i '' '/git push origin master/d' ~/.bash_profile
+printf "Git Shortcuts are gone\n\n"
+
+printf "Reloading .bash_profile\n"
+source ~/.bash_profile
+printf "Done!\n\n"
