6a9309bbd2a0619a7e6eea1c6a5b2c88bb1bb161
Flávio Silveira <flavioaugustosilveira@gmail.com>
2019-06-21 12:53:39 -0300

    Removing ggrep, start using ripgrep

--- scripts/gnu-softwares.sh
+++ scripts/gnu-softwares.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-if  [[ $(command brew ls | grep grep) != "" ]] && [[ "$1" != "-f" ]]
-then
-  printf "Oops! You already have Grep (via Homebrew) installed!\nUse -f as last parameter to force an update!\n\n" 
-else
-  if [[ $(command brew ls | grep grep) != "" ]]
-  then
-    printf "Upgrading Grep (via Homebrew) version...\n"
-    brew upgrade grep
-    printf "Grep (via Homebrew) is up to date!\n"
-  else
-    printf "Installing Grep (via Homebrew) version...\n"
-    brew install grep
-    printf "Grep (via Homebrew) installed!\n"
-  fi
-fi
-
 if  [[ $(command brew ls | grep gnu-sed) != "" ]] && [[ "$1" != "-f" ]]
 then
   printf "Oops! You already have Gnu-sed (via Homebrew) installed!\nUse -f as last parameter to force an update!\n\n" 
--- scripts/remove-gnu-softwares.sh
+++ scripts/remove-gnu-softwares.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-if  [[ $(command brew ls | grep grep) != "" ]]
-then
-  printf "Removing grep (via Homebrew)..\n"
-  brew remove --force grep
-  printf "grep (via Homebrew) was removed!\n\n"
-else
-  printf "Looks like grep (via Homebrew) is not installed.. Everything good here!\n\n"
-fi
-
 if  [[ $(command brew ls | grep gnu-sed) != "" ]]
 then
   printf "Removing gnu-sed (via Homebrew)..\n"
