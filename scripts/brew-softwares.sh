80f7e231cb16d9a98918b4a295d03b316cd31151
Flávio Silveira <flavioaugustosilveira@gmail.com>
2019-09-18 14:08:41 -0300

    Add sript part install mysql client

--- scripts/brew-softwares.sh
+++ scripts/brew-softwares.sh
@@ -31,3 +31,20 @@ else
     printf "Vim (via homebrew) installed!\n"
   fi
 fi
+
+if  [[ $(command brew ls | grep mysql-client) != "" ]] && [[ "$1" != "-f" ]]
+then
+  printf "Oops! You already have Mysql-client (via homebrew) installed!\nUse -f as last parameter to force an update!\n\n" 
+else
+  if [[ $(command brew ls | grep mysql-client) != "" ]]
+  then
+    printf "Upgrading Mysql-client (via homebrew) version...\n"
+    brew upgrade mysql-client
+    printf "Mysql-client (via homebrew) is up to date!\n"
+  else
+    printf "Installing Mysql-client (via homebrew) version...\n"
+    brew install mysql-client
+    echo 'export PATH="/usr/local/opt/mysql-client/bin:$PATH"' >> ~/.bash_profile
+    printf "Mysql-client (via homebrew) installed!\n"
+  fi
+fi
