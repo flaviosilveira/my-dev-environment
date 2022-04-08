6c6bcd52c720c6e7b77e4204a0cb65bb4a741c1b
Flávio Augusto da Silveira <flavioaugustosilveira@gmail.com>
2022-04-08 10:44:37 -0300

    Update git-shortcuts.sh

--- scripts/git-shortcuts.sh
+++ scripts/git-shortcuts.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-if [ ! -e ~/.bash_profile ]
+if [ ! -e ~/.zprofile ]
 then
-  printf "Cant find .bash_profile file.. creating one..\n"
-  touch ~/.bash_profile
-  chmod 644 ~/.bash_profile
-  printf ".bash_profile created!\n\n"
+  printf "Cant find .zprofile file.. creating one..\n"
+  touch ~/.zprofile
+  chmod 644 ~/.zprofile
+  printf ".zprofile created!\n\n"
 fi
 
-if  [[ $(command grep -il "git commit" ~/.bash_profile) != "" ]] && [[ "$1" != "-f" ]]
+if  [[ $(command grep -il "git commit" ~/.zprofile) != "" ]] && [[ "$1" != "-f" ]]
 then
-  printf "Looks like you already have some git shortcuts in your bash profile..\nUse -f to remove them and re-add them.\n\n"
+  printf "Looks like you already have some git shortcuts in your Z profile..\nUse -f to remove them and re-add them.\n\n"
 else
 
-  if [[ $(command grep -il "git commit" ~/.bash_profile) != "" ]]
+  if [[ $(command grep -il "git commit" ~/.zprofile) != "" ]]
   then
     printf "Removing actual git shortcuts..."
     ./scripts/remove-git-shortcuts.sh
   fi
 
   printf "Creating shortcuts...\n"
-  echo "" >> ~/.bash_profile
-  echo "## Git Shortcuts ###" >> ~/.bash_profile
-  echo "alias gs='git status'" >> ~/.bash_profile
-  echo "alias ga='git add'" >> ~/.bash_profile
-  echo "alias gd='git diff'" >> ~/.bash_profile
-  echo "alias gc='git commit'" >> ~/.bash_profile
+  echo "" >> ~/.zprofile
+  echo "## Git Shortcuts ###" >> ~/.zprofile
+  echo "alias gs='git status'" >> ~/.zprofile
+  echo "alias ga='git add'" >> ~/.zprofile
+  echo "alias gd='git diff'" >> ~/.zprofile
+  echo "alias gc='git commit'" >> ~/.zprofile
+  echo "alias gp='git pull'" >> ~/.zprofile
+  echo "alias gch='git checkout'" >> ~/.zprofile
   printf "Shortcuts created!\n\n"
 
 fi
